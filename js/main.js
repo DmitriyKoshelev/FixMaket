edd8efdf3603dee41d4da25a53634a9c6748e9bf
koshelev <koshelev.dmitryy@gmail.com>
2018-04-18 00:43:01 +0400

    fixed js

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(function () {
+	let windowWidth = $(window).width();
+    if (windowWidth>=992) {
+    	$(".header_top_menu").appendTo(".header_top_left");
+    	$(".header_top_search").appendTo(".header_top_inner");
+    	$(".header_bottom_menu").appendTo(".header_bottom_left_flexItem");
+    	$(".header_bottom_basket").appendTo(".header_bottom_inner");
+    }
+   
+    $('.header_burger_right').on("click", function () {
+        $(".mob_menu").stop().slideToggle("slow");
+    });
+    $(function() {
+  $('.select').niceSelect();
+});
+        $(".acordion_item_title").on("click",function(){
+            var acordionItem = $(this).closest(".acordion_item");
+            var acordionItemSiblings = $(acordionItem).siblings(".acordion_item");
+            var accordionCheckboxWrap = $(acordionItemSiblings).children(".acordion_checkbox_wrap");
+            $(accordionCheckboxWrap).hide();
+            $(this).siblings(".acordion_checkbox_wrap").slideToggle();
+
+        });
+
+    $('.select_filters').on("click", function () {
+        $(".main_filtres, .overlay").addClass("active");
+    });
+    $('.cross').on("click", function () {
+        $(".main_filtres, .overlay").removeClass("active")
+    });
+});
